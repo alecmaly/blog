718abeec46e86f332af37bb5e21fcd4426cc1cce
Alec Maly <32174181+alecjmaly@users.noreply.github.com>
2021-10-16 23:32:00 -0400

    Added lightbox images

--- serve.sh
+++ serve.sh
@@ -1,3 +1,4 @@
 python3 tag_generator.py
 bundle exec jekyll clean
-JEKYLL_ENV=production bundle exec jekyll serve --drafts --host=192.168.1.36 --livereload --profile
+# JEKYLL_ENV=production bundle exec jekyll serve --drafts --host=192.168.1.36 --livereload --profile
+JEKYLL_ENV=production bundle exec jekyll serve --drafts --host=127.0.0.1 --livereload --profile
